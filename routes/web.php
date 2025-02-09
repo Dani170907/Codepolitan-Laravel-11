e7551aeb471db606986cd51271ba3b671aa199b6
Dani170907 <daniramadhan170907@gmail.com>
2025-02-09 16:30:14 +0700

    learning method PUT

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,22 @@ Route::get('/movie', function () use ($movies) {
     return $movies;
 });
 
+
 Route::post('/movie', function () use ($movies) {
     $movies[] = [
         'title' => request('title'),
         'year' => request('year'),
         'genre' => request('genre'),
     ];
+
+    return $movies;
+});
+
+
+Route::put('/movie', function () use ($movies) {
+    $movies[2]['title'] = request('title');
+    $movies[2]['year'] = request('year');
+    $movies[2]['genre'] = request('genre');
 
     return $movies;
 });
