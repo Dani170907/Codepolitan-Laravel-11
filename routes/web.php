97f253c8301f63e48e45bc46a0be8d44359faba8
Dani170907 <daniramadhan170907@gmail.com>
2025-02-09 15:47:34 +0700

    learning method POST

--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/movie', function (){
-    $movies = [];
+$movies = [];
 
-    for ($i=0; $i < 10; $i++) {
-        $movies[] = [
-            'title' => 'Movie ' . $i,
-            'year' => '2020',
-            'genre' => 'Comedi',
-        ];
-    }
+for ($i = 0; $i <= 5; $i++) {
+    $movies[] = [
+        'title' => 'Movie ' . $i,
+        'year' => '2020',
+        'genre' => 'Comedi',
+    ];
+}
 
-    echo '<h1>Movies</h1>';
-    echo '<ul>';
-    foreach ($movies as $movie) {
-        echo '<li>' . $movie['title'] . '-' . $movie['year'] . '-' . $movie['genre']. '-' . '</li>';
-    }
-    echo '</ul>';
+Route::get('/movie', function () use ($movies) {
+
+    return $movies;
+});
+
+Route::post('/movie', function () use ($movies) {
+    $movies[] = [
+        'title' => request('title'),
+        'year' => request('year'),
+        'genre' => request('genre'),
+    ];
+
+    return $movies;
 });
